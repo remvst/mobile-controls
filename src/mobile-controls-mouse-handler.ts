05455cb2ecfea52331cefa8ec1fe19433313d54b
remvst <remvst@gmail.com>
2024-06-19 13:13:45 -0400

    Play it safe with pointer lock

--- src/mobile-controls-mouse-handler.ts
+++ src/mobile-controls-mouse-handler.ts
@@ -1,4 +1,4 @@
-import { distance } from "@remvst/geometry";
+import { Vector2Like, distance } from "@remvst/geometry";
 import { Button } from "./button";
 import { Control } from "./control";
 import { MobileControls } from "./mobile-controls";
@@ -14,50 +14,57 @@ export class MobileControlsMouseHandler {
         private readonly element: HTMLElement,
     ) {}
 
+    updateHoverState(position: Vector2Like) {
+        this.hoveringControl = null;
+
+        for (const control of this.controls.controls) {
+            if (
+                control.enabled &&
+                control instanceof Button &&
+                distance(control.view.position, position) <
+                    control.radius
+            ) {
+                this.hoveringControl = control;
+                break;
+            }
+        }
+
+        for (const control of this.controls.controls) {
+            if (control instanceof Button) {
+                control.hovered = control === this.hoveringControl;
+            }
+        }
+
+        this.element.style.cursor = this.hoveringControl
+            ? "pointer"
+            : "inherit";
+    }
+
     setup() {
         let mouseDown = false;
 
         // Handle cursor changes
         this.listeners.push(
             addEventListener(document.body, "mousemove", (event) => {
-                this.hoveringControl = null;
-
-                const position = { x: event.pageX, y: event.pageY };
-                for (const control of this.controls.controls) {
-                    if (
-                        control.enabled &&
-                        control instanceof Button &&
-                        distance(control.view.position, position) <
-                            control.radius
-                    ) {
-                        this.hoveringControl = control;
-                        break;
-                    }
-                }
-
-                for (const control of this.controls.controls) {
-                    if (control instanceof Button) {
-                        control.hovered = control === this.hoveringControl;
-                    }
-                }
-
-                this.element.style.cursor = this.hoveringControl
-                    ? "pointer"
-                    : "inherit";
+                if (document.pointerLockElement) return;
+                this.updateHoverState({ x: event.pageX, y: event.pageY });
             }),
             addEventListener(document.body, "mousedown", (event) => {
+                if (document.pointerLockElement) return;
                 mouseDown = true;
                 mapAndUpdateTouches(this.element, this.controls, [
                     event,
                 ] as any as TouchList);
             }),
             addEventListener(document.body, "mousemove", (event) => {
+                if (document.pointerLockElement) return;
                 if (mouseDown)
                     mapAndUpdateTouches(this.element, this.controls, [
                         event,
                     ] as any as TouchList);
             }),
             addEventListener(document.body, "mouseup", (event) => {
+                if (document.pointerLockElement) return;
                 mouseDown = false;
                 mapAndUpdateTouches(
                     this.element,
