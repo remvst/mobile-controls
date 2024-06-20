65a0151621256a77243fe5e901142510b0b3a9c1
remvst <remvst@gmail.com>
2024-06-20 10:31:07 -0400

    Use JS private

--- src/mobile-controls.ts
+++ src/mobile-controls.ts
@@ -8,11 +8,10 @@ export abstract class MobileControls {
 
     readonly stage = new PIXI.Container();
 
-    private readonly claimMap = new Map<number, Control>();
+    readonly #claimMap = new Map<number, Control>();
+    readonly #previousTouchIdentifiers = new Set<number>();
 
     readonly controls: Control[] = [];
-
-    private readonly previousTouchIdentifiers = new Set<number>();
 
     abstract updateLayout(width: number, height: number): void;
 
@@ -29,7 +28,7 @@ export abstract class MobileControls {
     add<ControlType extends Control>(control: ControlType): ControlType {
         this.controls.push(control);
         this.stage.addChild(control.view);
-        control.update([], this.previousTouchIdentifiers);
+        control.update([], this.#previousTouchIdentifiers);
 
         return control;
     }
@@ -50,12 +49,12 @@ export abstract class MobileControls {
 
         const seenIdentifiers = new Set<number>();
         for (const touch of touches) {
-            touch.claimedBy = this.claimMap.get(touch.identifier) || null;
+            touch.claimedBy = this.#claimMap.get(touch.identifier) || null;
             seenIdentifiers.add(touch.identifier);
         }
 
         for (const control of this.controls) {
-            control.update(touches, this.previousTouchIdentifiers);
+            control.update(touches, this.#previousTouchIdentifiers);
 
             // In case this specific control caused destroy() to be called, make sure we stop
             if (this.stage.destroyed) break;
@@ -63,19 +62,19 @@ export abstract class MobileControls {
 
         for (const touch of touches) {
             if (touch.claimedBy) {
-                this.claimMap.set(touch.identifier, touch.claimedBy);
+                this.#claimMap.set(touch.identifier, touch.claimedBy);
             }
         }
 
-        for (const identifier of this.claimMap.keys()) {
+        for (const identifier of this.#claimMap.keys()) {
             if (!seenIdentifiers.has(identifier)) {
-                this.claimMap.delete(identifier);
+                this.#claimMap.delete(identifier);
             }
         }
 
-        this.previousTouchIdentifiers.clear();
+        this.#previousTouchIdentifiers.clear();
         for (const touch of touches) {
-            this.previousTouchIdentifiers.add(touch.identifier);
+            this.#previousTouchIdentifiers.add(touch.identifier);
         }
     }
 }
